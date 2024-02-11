6791cb21f2f0991f643b6b5c33aa0b36287d289c
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-02-11 17:37:52 +0500

    initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-
 // Index page
 try {
     const registerButtons = document.querySelectorAll('.webinar-main-button');
     const modalBackdrop = document.querySelector('.modal-backdrop');
     const modalCloserElements = document.querySelectorAll('[data-modal-close]');
     const form = document.querySelector('.form');
+    const formAlert = document.querySelector('.form-alert');
 
     registerButtons.forEach(button => {
         button.addEventListener('click', () => {
@@ -39,6 +39,18 @@ try {
         const name = e.target.querySelector('#name').value.trim();
         const phone = e.target.querySelector('#phone').value?.replace(/[^0-9]/g, '');
 
+        if (!name.length) {
+            formAlert.textContent = 'Ismingizni kiriting';
+            formAlert.classList.add('open');
+            return;
+        }
+
+        if (phone?.length !== 12) {
+            formAlert.textContent = 'Telefon raqamingizni kiriting';
+            formAlert.classList.add('open');
+            return;
+        }
+
         if (name.length && phone?.length === 12) {
             submitButton.setAttribute('disabled', true);
             submitButton.textContent = 'Yuborilmoqda...'
@@ -49,6 +61,7 @@ try {
                 time: new Date().toLocaleString()
             }))
 
+            formAlert.classList.remove('open');
             window.location.href = `${window.location.protocol}//${window.location.host}/telegram.html`
         }
     })
